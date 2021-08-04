46870972e62503dd66dcfeaffb2d9d4baa29d8b8
Roman Zimnik <roman@software-crafter.com>
2021-08-05 00:28:13 +0200

    structure added

--- main.go
+++ main.go
@@ -5,6 +5,11 @@ import (
 	"os"
 )
 
+type Saiyan struct {
+	Name  string
+	Power int
+}
+
 func main() {
 	if len(os.Args) != 2 {
 		os.Exit(1)
@@ -14,4 +19,31 @@ func main() {
 	for i := 0; i < 2; i++ {
 		fmt.Println(os.Args[i])
 	}
+
+	log("test")
+	fmt.Println(add(2, 3))
+
+	val, bol := power("test")
+	fmt.Println(val)
+	fmt.Println(bol)
+
+	goku := Saiyan{
+		Name:  "Goku",
+		Power: 9000,
+	}
+
+	fmt.Println(goku.Name)
+	fmt.Println(goku.Power)
+}
+
+func log(message string) {
+	fmt.Println(message)
+}
+
+func add(a int, b int) int {
+	return a + b
+}
+
+func power(name string) (string, bool) {
+	return name, true
 }
